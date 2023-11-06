2c13f1c6833966a6f1f20d526e49770e092da081
madspecco <bogdan.bolos30@gmail.com>
2023-11-06 16:06:16 +0200

    styled activity-screen
    basic styling with respect to the color scheme

--- ui/src/app/activity-screen/activity-screen.component.ts
+++ ui/src/app/activity-screen/activity-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-activity-screen',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./activity-screen.component.css']
 })
 export class ActivityScreenComponent {
+  constructor(private router: Router) {}
 
+  goTo(destination: string) {
+    this.router.navigate([`/${destination}`]);
+  }
 }
