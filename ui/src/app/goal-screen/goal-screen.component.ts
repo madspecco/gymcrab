600bd8cab09a30ebd2891c5dac3be52e5c3ea008
madspecco <bogdan.bolos30@gmail.com>
2023-11-06 15:50:06 +0200

    created and styled goal-screen
    goal-screen is where the user will choose his goal accordingly
    
    (also modified index.html to hide any overflow)

--- ui/src/app/goal-screen/goal-screen.component.ts
+++ ui/src/app/goal-screen/goal-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-goal-screen',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./goal-screen.component.css']
 })
 export class GoalScreenComponent {
+  constructor(private router: Router) {}
 
+  goBack() {
+    this.router.navigate(['/']);
+  }
 }
