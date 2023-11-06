882806fd548d1572e250b41135c844cfd8a92b75
madspecco <bogdan.bolos30@gmail.com>
2023-11-06 16:33:18 +0200

    styled biometrics-screen
    basic styling, needs revision

--- ui/src/app/biometrics-screen/biometrics-screen.component.ts
+++ ui/src/app/biometrics-screen/biometrics-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-biometrics-screen',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./biometrics-screen.component.css']
 })
 export class BiometricsScreenComponent {
+  constructor(private router: Router) {}
 
+  goTo(destination: string) {
+    this.router.navigate([`/${destination}`]);
+  }
 }
