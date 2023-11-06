aa53c5fffe5cac086448ee4985ddbbdef01284fb
madspecco <bogdan.bolos30@gmail.com>
2023-11-06 15:22:54 +0200

    back button functionality

--- ui/src/app/signup-screen/signup-screen.component.ts
+++ ui/src/app/signup-screen/signup-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-signup-screen',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup-screen.component.css']
 })
 export class SignupScreenComponent {
+  constructor(private router: Router) {}
 
+  goBack() {
+    this.router.navigate(['/']);
+  }
 }
